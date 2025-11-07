8c44e19078be7a704e3589c3d823186757f48dcd
Samarth Dhoble <samarthdhoble2005@gmail.com>
2025-11-07 22:31:02 +0530

    Delete comment feature is added

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -140,3 +140,37 @@ const get_comments_by_post = async (req , res) => {
   }
 
 };
+
+const delete_comment_of_user = async (req , res) => {
+  const { token , comment_id } = req.body;
+  try {
+
+
+    const user = await User.findOne({token : token}).select('_id');
+
+
+    if (!user) {
+      return res.status(404).json({message : 'User not found!'});
+    } 
+
+    const comment = await Comment.findOne({_id : comment_id});
+
+    if (!comment) {
+      return res.status(404).json({message : 'Comment not found!'});
+    }
+
+    if (comment.userId.toString() !== user._id.toString()) {
+      return res.status(403).json({message : 'You are not authorized to delete this comment!'});
+    }
+
+    await Comment.deleteOne({_id : comment_id});
+
+    return res.status(200).json({message : 'Comment deleted successfully!'});
+
+  } catch(err) {  
+    return res.status(500).json({message : 'Internal Server Error', error : err.message});
+  }
+
+}
+
+
